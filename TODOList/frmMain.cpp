bf8aefbbd29d34db9182f6fb41f57a8e355a1899
aleksandr.galiew@gmail.com <aleksandr.galiew@gmail.com>
2025-05-17 09:19:52 +0300

    Добавьте файлы проекта.

---
+++ TODOList/frmMain.cpp
@@ -0,0 +1,68 @@
+#include "frmMain.h"
+
+#include <Windows.h>
+#include <iostream>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void CODE() {
+
+}
+
+void APPLICATION_START() {
+	setlocale(LC_ALL, "RU_ru");
+	system("color 70");
+	std::cout << "\n APPLICATION START\n\n";
+	std::cout << "\tStatistic:\n\n";
+
+	// Let's code
+	std::cout << "\tInfo of Application here =/\n";
+	CODE();
+
+	std::cout << "\n\n";
+}
+void APPLICATION_FORM() {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	TODOList::frmMain form;
+	Application::Run(% form);
+}
+void APPLICATION_END() {
+	std::cout << "\n APPLICATION END\n\n";
+	system("pause");
+}
+
+/// ver 0.1.0
+int main(array<String^>^ args) {
+	_CrtSetDbgFlag(
+		_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) |
+		_CRTDBG_LEAK_CHECK_DF);
+
+
+	APPLICATION_START();
+	APPLICATION_FORM();
+	APPLICATION_END();
+	return 0;
+}
+
+
+
+/// TODO:
+// 
+// (+) MVP
+// (+) GIT Connection
+// (+) GIT Make "Develop" branch
+// 
+// -------------------
+// ( ) Task
+// -------------------
+// ( ) Task
+// ( ) Task
+//
+//
+/// BUGS:
+//
+// ( ) Component: the essence of the problem
+// ( ) 
